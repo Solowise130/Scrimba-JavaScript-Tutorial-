92615b9c83050180f1edaad843d9c144473ef445
Solowise130 <iniodusolomon@gmail.com.com>
2023-11-06 12:16:54 +0100

    JS tutorial.

---
+++ JS/dom.js
@@ -0,0 +1,20 @@
+//CHALLENGE
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+//SOLUTION
+
+function save() {
+
+    console.log(count);
+}
+
+save();
---
+++ JS/string.js
@@ -0,0 +1,27 @@
+
+// Create a variable, message, that stores the string: "You have tree new notifications"
+
+//SOLUTION
+//let message = 'You have three new notification';
+//console.log(message);
+
+//CHALLENGE TWO
+// Create a variable, messageToUser, that contains the message we have logged
+
+//SOLUTION
+//let messageToUser = 'You have three new notifications. Pers!';
+//.log(messageToUser);
+
+//CHALLENGE THREE
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+//SOLUTION
+let greeting = 'Hello everyone, my name is  ';
+let name = 'Solomon Iniodu';
+let myGreeting = greeting  + name;
+
+console.log(myGreeting);
